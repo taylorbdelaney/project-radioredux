b3e0d2788d776f9bde7b8001b401a0e7b004cd49
taylorbdelaney <taylor_delaney@mac.com>
2016-04-26 11:49:47 -0400

    Register links to db
    Still need to fix a few things but working overall

--- include/login.php
+++ include/login.php
@@ -6,43 +6,81 @@
 		die('Connect Error (' . $mysqli->connect_errno . '): ' . $mysqli->connect_error);
 	}
 	
-	$userEmail = $_POST['email'];
-	$userPassword = $_POST['pass'];
+	$action = "";
 	
-	$validEmail = checkEmail($userEmail, $mysqli);
+	if (isset($_POST['login'])) {
 	
-	if ($validEmail = TRUE) {
-		$validPass = checkPass($userPassword, $userEmail, $mysqli);
+		$action = 'login';
 		
-		if ($validPass = TRUE) {
-			$userq = "SELECT * FROM users WHERE email = '$userEmail'";
-			$result = $mysqli->query($userq);
+		$userEmail = $_POST['email'];
+		$userPassword = $_POST['pass'];
+	
+		$validEmail = checkEmail($userEmail, $mysqli, $action);
+	
+		if ($validEmail = TRUE) {
+			$validPass = checkPass($userPassword, $userEmail, $mysqli);
+		
+			if ($validPass = TRUE) {
+				$userq = "SELECT * FROM users WHERE email = '$userEmail'";
+				$result = $mysqli->query($userq);
 			
-			if ($result->num_rows == 0) {
-				die("Bad query $sql");
-			}
-			$user_data = array();
-			while ($obj = mysqli_fetch_object($result)) {
-				$user_data[] = $obj;
-			}
-			$jsonUser = json_encode($user_data);
+				if ($result->num_rows == 0) {
+					die("Bad query $sql");
+				}
+				$user_data = array();
+				while ($obj = mysqli_fetch_object($result)) {
+					$user_data[] = $obj;
+				}
+				$jsonUser = json_encode($user_data);
 			
-			header('Location: http://cscilab.bc.edu/~delanetc/radioRedux/index.html');
+				header('Location: http://cscilab.bc.edu/~delanetc/radioRedux/index.html');
+			}
 		}
+	}
+	
+	if (isset($_POST['submitReg'])) {
+	
+		$action = 'register';
+		
+		$firstName = $_POST['first_name'];
+		$lastName = $_POST['last_name'];
+		$emailReg = $_POST['emailReg'];
+		$passReg = $_POST['passwordReg'];
+		
+		$newEmail = checkEmail($emailReg, $mysqli, $action);
+		if ($newEmail == TRUE) {
+			//Fix id and registration_date
+			$insert = "INSERT INTO users (registration_date, first_name, last_name, email, password) VALUES (DATE(NOW()), '$firstName', '$lastName', '$emailReg', SHA1('$passReg'))";
+			
+			if ($mysqli->query($insert) === TRUE) {
+				header('Location: ../index.html');
+			} else {
+				echo "Error: $insert <br>" . $mysqli->error;
+			}
+		}
+	
 	}
 
 
 
 	$mysqli->close();
 	
-	function checkEmail($userEmail, $mysqli) {
+	function checkEmail($userEmail, $mysqli, $action) {
 		$emailq = "SELECT email FROM users WHERE email = '$userEmail'";
 		$result = $mysqli->query($emailq);
 		
-		if ($result->num_rows !== 1) {
-			die("This email address does not exist in our user database");
-		} else {
-			return $emailCheck = TRUE;
+		if ($action == 'login') {
+			if ($result->num_rows !== 1) {
+				die("This email address does not exist in our user database");
+			} else {
+				return $emailCheck = TRUE;
+			}
+		} else if ($action == 'register') {
+			if ($result->num_rows !== 0) {
+				die("This email address already exists in our database. <a href='../account/register.html'>Click here</a> to return to registration.");
+			} else {
+				return $emailCheck = TRUE;
+			}
 		}
 	}
 	function checkPass($userPassword, $userEmail, $mysqli) {
